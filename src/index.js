3359c81aac6ce4c5fd9bd7d81cd49594263df651
Anna Potapova <anna.potapova@rndx2.com>
2022-03-18 18:57:18 +0300

    feat: added dispatch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function createStore(initialState) {
+    let state = initialState;
+
+    function getState() {
+        return state;
+    }
+    function dispatch(action) {
+        if (action.type === "task/completed") {
+            const newArray = [...state];
+            const elementIndex = newArray.findIndex(
+                (el) => el.id === action.payload.id
+            );
+            newArray[elementIndex].completed = true;
+            state = newArray;
+            console.log(state);
+        }
+    }
+
+    return { getState, dispatch };
+}
+const store = createStore([{ id: 1, descriptiob: "Task 1", completed: false }]);
+
+const App = (params) => {
+    console.log(store.getState());
+    return (
+        <>
+            <h1>App</h1>
+            <button
+                onClick={() =>
+                    store.dispatch({
+                        type: "task/completed",
+                        payload: { id: 1 }
+                    })
+                }
+            >
+                Complete
+            </button>
+        </>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
